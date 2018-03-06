6b86e2e6d262665ca71d0c8c4d1763847eafe0a5
luiszhou <luis.zhou.zw@gmail.com>
2018-03-06 09:46:25 +0800

    update

--- console/console_test.go
+++ console/console_test.go
@@ -3,10 +3,17 @@ package console
 import (
 	"github.com/LuisZhou/lpge/conf"
 	"github.com/LuisZhou/lpge/console"
+	"sync"
 	"testing"
 )
 
 func TestConsole(t *testing.T) {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
 	conf.ConsolePort = 6001
 	console.Init()
+
+	wg.Wait()
 }
