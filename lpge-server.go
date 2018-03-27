074754e8dbe306f40178cf6a479df8ab9b931059
wangqian <luis.zhou.zw@gamil.com>
2018-03-27 18:57:10 +0800

    update

--- lpge-server.go
+++ lpge-server.go
@@ -3,7 +3,44 @@ package main
 import (
 	"fmt"
 	"github.com/LuisZhou/lpge/conf"
+	"github.com/LuisZhou/lpge/log"
+	"github.com/LuisZhou/lpge/module"
 )
+
+func Run(mods ...module.Module) {
+	// logger
+	if conf.LogLevel != "" {
+		logger, err := log.New(conf.LogLevel, conf.LogPath, conf.LogFlag)
+		if err != nil {
+			panic(err)
+		}
+		log.Export(logger)
+		defer logger.Close()
+	}
+
+	log.Release("Leaf %v starting up", version)
+
+	// module
+	for i := 0; i < len(mods); i++ {
+		module.Register(mods[i])
+	}
+	module.Init()
+
+	// cluster
+	cluster.Init()
+
+	// console
+	console.Init()
+
+	// close
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	log.Release("Leaf closing down (signal: %v)", sig)
+	console.Destroy()
+	cluster.Destroy()
+	module.Destroy()
+}
 
 func main() {
 	fmt.Println(conf.LenStackBuf)
