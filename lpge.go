b1ecf03dfdaee303ab863f0c4631c9f256c42c75
luiszhou <luis_zhou@twinstargame.com>
2018-06-02 19:12:53 +0800

    update

--- lpge.go
+++ lpge.go
@@ -6,9 +6,23 @@ import (
 	"github.com/LuisZhou/lpge/log"
 	"github.com/LuisZhou/lpge/module"
 	"github.com/LuisZhou/lpge/orm"
+	"github.com/jinzhu/gorm"
 	"os"
 	"os/signal"
 )
+
+func Migrate(f func(*gorm.DB)) {
+	if conf.DBConfig.Default != "" {
+		conns := make(map[string]string)
+		conns["mysql"] = conf.DBConfig.Mysql
+		conns["postgres"] = conf.DBConfig.Postgres
+		conns["sqlite"] = conf.DBConfig.Sqlite
+		conns["mssql"] = conf.DBConfig.Mssql
+		orm.Connect(conns, conf.DBConfig.Default)
+
+		f(orm.Default())
+	}
+}
 
 func Run(mods map[string]module.Module) {
 	// logger
@@ -19,15 +33,6 @@ func Run(mods map[string]module.Module) {
 		}
 		log.Export(logger)
 		defer logger.Close()
-	}
-
-	if conf.DBConfig.Default != "" {
-		conns := make(map[string]string)
-		conns["mysql"] = conf.DBConfig.Mysql
-		conns["postgres"] = conf.DBConfig.Postgres
-		conns["sqlite"] = conf.DBConfig.Sqlite
-		conns["mssql"] = conf.DBConfig.Mssql
-		orm.Connect(conns, conf.DBConfig.Default)
 	}
 
 	log.Release("LPGE %v starting up", 1.0)
